181956ff1a84a1981437fe55d5f07932819a4983
unknown <Shikder-Vai>
2022-04-18 23:23:42 +0600

    some more added

--- src/hooks/useProduct.js
+++ src/hooks/useProduct.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from "react";
 
-const useProduct = () => {
-    const [services,setServices] =useState([])
-    useEffect(() =>{
-        fetch("services.json")
-        .then(res => res.json())
-        .then(data => setServices(data))
-    },[])
-    return [services, setServices];
-};
+// const useProduct = () => {
+//     const [services,setServices] =useState([])
+//     useEffect(() =>{
+//         fetch("services.json")
+//         .then(res => res.json())
+//         .then(data => setServices(data))
+//     },[])
+//     return [services, setServices];
+// };
 
-export default useProduct;
+// export default useProduct;
