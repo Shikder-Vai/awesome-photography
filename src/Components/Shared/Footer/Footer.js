0703562a5d3b208e2f7782234f9dbb3b2e729638
unknown <Shikder-Vai>
2022-04-18 20:18:20 +0600

    header , footer added

---
+++ src/Components/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../CustomCss/Custom.css"
+
+const Footer = () => {
+    return (
+        <div className='d-inline-block w-100 mt-5 light-color-background'>
+           <h1 className='text-muted my-5 text-center'>this is footer</h1> 
+        </div>
+    );
+};
+
+export default Footer;
