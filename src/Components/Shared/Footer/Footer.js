c24fcbe543080b5026c56c67f3b9429ef8c5f064
unknown <Shikder-Vai>
2022-04-18 23:43:53 +0600

    footer

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import "../CustomCss/Custom.css"
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div className='d-inline-block w-100 mt-5 light-color-background'>
-           <h1 className='text-muted my-5 text-center'>this is footer</h1> 
+        <div className='d-inline-block w-100 mt-5 light-color-background d-flex text-secondary'>
+          <div className='my-5 mx-auto'>
+              <p className='pt-3 pb-2'>Awesome Photography Centre</p>
+              <p>copyright @ AwesomePhotography</p>
+          </div>
+          <div className='my-5 mx-auto'>
+              <h4>Link</h4>
+              <Link className='text-decoration-none text-secondary' to="/home">HOME</Link>
+              <br />
+              <Link className='text-decoration-none text-secondary' to="/blogs">BLOGS</Link>
+              <br />
+              <Link className='text-decoration-none text-secondary' to="/register">REGISTER</Link>
+              <br />
+              <Link className='text-decoration-none text-secondary' to="/login">LOGIN</Link>
+              </div>
         </div>
     );
 };
