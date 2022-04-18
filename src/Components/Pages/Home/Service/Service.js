12555dd289c6c9666e6ee615a1f605fd9997b30a
unknown <Shikder-Vai>
2022-04-18 21:30:27 +0600

    some responsive done

--- src/Components/Pages/Home/Service/Service.js
+++ src/Components/Pages/Home/Service/Service.js
@@ -6,25 +6,24 @@ import "../../../Shared/CustomCss/Custom.css";
 const Service = ({ service }) => {
   const { name, price, description, img } = service;
   return (
-    <div class="col shadow px-0 bg-body rounded">
-      <div class="card h-100">
-        <img src={img} class="card-img-top" alt="..." />
-        <div class="card-body" >
-          <h5 class="card-title">{name}</h5>
-          <p class="card-text">
-            {description}
-          </p>
-        </div>
-        <h5 className="ps-4">Price: {price}</h5>
-        <div >
-          <Button className="button-style w-100">
-            <Link className="text-decoration-none text-light" to="/checkout">
-              Checkout
-            </Link>
-          </Button>
-        </div>
+    <div>
+      <div className="card h-100 col shadow  rounded py-2">
+      <img src={img} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+      </div>
+      <h5 className="ps-4">Price: {price}</h5>
+      <div>
+        <Button className="button-style w-100">
+          <Link className="text-decoration-none text-light" to="/checkout">
+            Checkout
+          </Link>
+        </Button>
       </div>
     </div>
+    </div>
+    
   );
 };
 
